fe3aca108dd802d5e5a8f78301eb456b178f9a6d
Hun <98679546+hoonpak@users.noreply.github.com>
2024-08-21 09:51:48 +0900

    Update run_model_training_test.sh

--- script/run_model_training_test.sh
+++ script/run_model_training_test.sh
@@ -1,3 +1,3 @@
 cd "/hdd1/user19/bag/4.RNN/src"
 CUDA_VISIBLE_DEVICES=0 python train.py --no-reverse --no-dropout --no-input_feeding --sensitive --attn no --align no --name np_v4_base_sensitive --device cuda:0
-python test.py --no-reverse --no-dropout --no-input_feeding --sensitive --attn no --align no --name np_v4_base_sensitive --device cuda:0
+CUDA_VISIBLE_DEVICES=0 python test.py --no-reverse --no-dropout --no-input_feeding --sensitive --attn no --align no --name np_v4_base_sensitive --device cuda:0
